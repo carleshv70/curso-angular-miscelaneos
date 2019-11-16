d1df7745a8f3ccea1d862f0ff80bb58126fc188c
carleshv70 <carleshv70@gmail.com>
2019-11-16 07:37:15 +0100

    Actualitzar projecte

---
+++ src/app/components/ng-style/ng-style.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-ng-style',
+  template: `
+    <!--<p [ngStyle]="{'font-size': '15px'}">-->
+    <p [style.fontSize.px]="tamano">
+      Hola mundo... esto es una etiqueta.
+    </p>
+
+    <button class="btn btn-primary" (click)="tamano=tamano+5">
+<i class="fa fa-plus" aria-hidden="true"></i>
+    </button>
+
+    <button class="btn btn-primary" (click)="tamano=tamano-5">
+    <i class="fas fa-minus"></i>
+    </button>
+  `,
+  styles: []
+})
+export class NgStyleComponent implements OnInit {
+
+  tamano: number = 20;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
